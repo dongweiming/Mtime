0993d1544520aba099f9498063e3b60e1b96e8f9
dongweiming <ciici1234@hotmail.com>
2014-03-22 17:19:38 +0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+import models
+import parse
+from schedulers import Message
+
+def real_mapper(queryset):
+    this = Message.objects(task=queryset.task, payload=queryset.payload)
+    STATE = True
+    Model = getattr(models, queryset.task)
+    this.update(set__inprocess=True)
+    if queryset.task == 'Movie':
+        for process in queryset.payload:
+            ret = parse.get_movie_info(process)
+            models.Movie(ret).save()
+        return
+    Parse = getattr(parse, queryset.task + 'Parse')
+    for process in queryset.payload:
+        try:
+            p = Parse(process)
+            count = 1
+            while 1:
+                result, hasnext = p()
+                Model(**result).save()
+                # 别名体系, 这样只需要全局记录一个人物就知道他们的全部别名
+                for k, v in this._alias.items():
+                    models.AliasName.objects.get_or_create(
+                        name=k                    )[0].update(add_to_set__alias=v)
+                if hasnext:
+                    count += 1
+                    url = p.original_url
+                    p.set_url(url.replace('.html', '-{}.html'.format(count)))
+                else:
+                    #没有下一页就退出循环
+                    break
+        except:
+            STATE = False
+        else:
+            models.IdFinished.objects(
+                year=queryset.year            ).update(add_to_set__ids=[process])
+    if STATE:
+        this.update(set__state=2)
+    else:
+        this.update(set__state=3)
+    this.update(set__inprocess=False)
+
+all = Message.objects(state__ne=2, inprocess__ne=True)
+
+for i in all:
+    try:
+        real_mapper(i)
+    except:
+        raise
